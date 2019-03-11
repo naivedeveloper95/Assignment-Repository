73454d958c840e284db115213c8a8b32bdaf1181
satyam suman <naivedeveloper95@users.noreply.github.com>
2019-03-11 12:00:18 +0530

    First commit

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from 'src/test/test.component';
+import { Test } from 'src/test/test.services';
+import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
+import { TestLevel2Component } from 'src/test-level2/test-level2.component';
+
+const appRoutes: Routes = [
+    { path: '', component: TestComponent },
+    { path: 'level2', component: TestLevel2Component }
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        TestComponent,
+        TestLevel2Component
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(appRoutes),
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    providers: [
+        Test,
+        HttpClient,
+        HttpClientModule
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/test/test.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Test } from './test.services';
+
+@Component({
+	selector: 'app-test',
+	templateUrl: './test.component.html',
+	styleUrls: ['./test.component.css'],
+})
+export class TestComponent {
+	dataToDisplay: any = [];
+	arrayDataOne = [];
+	arrayDataTwo = [];
+	count: any = 0;
+	constructor (private test: Test) {
+		this.getDataToDisplay();
+	}
+
+	getDataToDisplay() {
+		this.test.getOrders().then((res: any) => {
+			this.dataToDisplay = res.rows;
+		});
+	}
+}
